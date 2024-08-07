d5b25783bf40ca36e1dbe1d889c581717229d9ed
任帅 <renshuai@renshuaideMacBook-Air.local>
2024-08-07 23:21:45 +0800

    feat: tab of js react vue

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
-import { testFunction } from './test/index.ts';
+const iframe = document.getElementById('content-iframe') as HTMLIFrameElement
+const links = document.querySelectorAll('.nav a')
 
-testFunction();
-
-function log(target: any, key: string, descriptor: PropertyDescriptor): PropertyDescriptor {
-    const originalMethod = descriptor.value
-
-    descriptor.value = function(...args: any[]) {
-        console.log(`Method ${key} is called, the params are ${JSON.stringify(args)}`)
-        return originalMethod.apply(this, args)
-    }
-
-    return descriptor
-}
-
-class Person{
-    name: string
-    age: number
-
-    constructor(name: string, age: number) {
-        this.name = name
-        this.age = age
-    }
-
-    @log
-    say(word: string) {
-        console.log(`Hello my name is ${this.name}, I am ${this.age} years old. I want to say ${word}.`)
-    }
-}
-
-const p = new Person('zs', 18)
-
-p.say('bala')
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault() // 阻止 a 标签 默认的行为
+        const src = link.getAttribute('src-data') || ''
+        iframe.src = src
+    })
+})
